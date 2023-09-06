83930a4b6b0099bcad03fb508f40551dba065b81
e-bertuzzi <e.bertuzzi003@studenti.unibs.it>
2023-09-06 17:20:21 +0200

    added new view features
    added new view features using bootstrap

--- appbooks/views.py
+++ appbooks/views.py
@@ -4,9 +4,6 @@ from appbooks.models import Reading
 
 # Create your views here.
 
-#def index(request):
-#    return render(request, 'index.html')
-
 def readings_list(request):
     readings = Reading.objects.all()
     return render(request, 'readings_list.html', {'readings': readings})
