b648490104dc2125983f3082b80b6944f961944e
e-bertuzzi <e.bertuzzi003@studenti.unibs.it>
2023-09-06 11:46:14 +0200

    Modified view using queryset
    change view file for home page (created reading_list.html and deleted index.html); in readings.html added table with database values through queryset

--- appbooks/views.py
+++ appbooks/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
+from appbooks.models import Reading
+
 # Create your views here.
 
-def index(request):
-    return render(request, 'index.html')
+#def index(request):
+#    return render(request, 'index.html')
+
+def readings_list(request):
+    readings = Reading.objects.all()
+    return render(request, 'readings_list.html', {'readings': readings})
