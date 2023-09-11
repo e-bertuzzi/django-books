e7bb0051bf5459cd1bb78af530d6a5153bc997ac
e-bertuzzi <e.bertuzzi003@studenti.unibs.it>
2023-09-11 18:23:12 +0200

    added the feature to sort the table

--- appbooks/views.py
+++ appbooks/views.py
@@ -8,8 +8,17 @@ from appbooks.models import Reading, Author
 
 @login_required(login_url="/login/")
 def readings_list(request):
+    order_by = request.GET.get('order_by')
+    if order_by is None:
+        order_by = 'data_inizio'
+
     today_seconds = datetime.today().timestamp() #positivo
-    readings = Reading.objects.all()
+
+    if order_by == 'titolo':
+        readings = Reading.objects.all().order_by('book__titolo')
+    else:
+        readings = Reading.objects.all().order_by(order_by)
+
     return render(request, 'readings_list.html', {'readings': readings, 'today_seconds': today_seconds})
 
 @login_required(login_url="/login/")
